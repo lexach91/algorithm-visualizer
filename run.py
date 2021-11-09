d8580a01b8b5aa6a718ad282bd81de018c2aaf63
lexach91 <konovalov1991@mail.ru>
2021-11-09 14:00:51 +0800

    Add constant variables for grid colors and dimensions

--- run.py
+++ run.py
@@ -1,4 +1,19 @@
-from blessed import Terminal
+from blessed import Terminal, terminal
 import random
 import numpy as np
 from time import sleep
+
+# create a Terminal instance
+terminal = Terminal()
+
+# Constants for the grid colors and dimensions
+WALL = "⬜️"
+PATH = "🟦"
+VISITED = "🟩"
+ACTIVE = "🟨"
+START = "🧐"
+END = "🏁"
+EMPTY = "　"
+
+WIDTH = 41
+HEIGHT = 31
